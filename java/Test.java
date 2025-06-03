a25debe1f0beed65180e3161beb97e9bd3b354ab
PPPig12315 <49577153+PPPig0132@users.noreply.github.com>
2025-06-03 20:03:27 +0800

    Revert "finished hot_150 bitoperation"

--- java/Test.java
+++ java/Test.java
@@ -9,10 +9,14 @@ public class Test {
         // for(int i=0;i<c1.length;++i){
         //     System.out.print(c1[i]+" ");
         // }
-       int x=5,y=7;
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(null);
         // int a=queue.poll();
-        // int b=queue.peek();
+        int b=queue.peek();
         
-        System.out.println(x&y); // Output: 4 (The longest valid parentheses substring is "()()".)
+        System.out.println(queue.isEmpty()); // Output: 4 (The longest valid parentheses substring is "()()".)
     }
 }
