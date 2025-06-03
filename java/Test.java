167a63c6fa3bf0ac655b143ef86a1a0aa556d15f
Your Name <you@example.com>
2025-06-03 20:23:12 +0800

    finished local changes

--- java/Test.java
+++ java/Test.java
@@ -9,6 +9,7 @@ public class Test {
         // for(int i=0;i<c1.length;++i){
         //     System.out.print(c1[i]+" ");
         // }
+<<<<<<< HEAD
        int x=15;
        Long res=289398L*289398L;
        for(int i=1;i<=x;++i){
@@ -20,5 +21,16 @@ public class Test {
         System.out.println(289398*289398);
         System.out.println(res);
          System.out.println(46340*46340); // Output: 4 (The longest valid parentheses substring is "()()".)
+=======
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(null);
+        // int a=queue.poll();
+        int b=queue.peek();
+        
+        System.out.println(queue.isEmpty()); // Output: 4 (The longest valid parentheses substring is "()()".)
+>>>>>>> parent of 3893e01 (Merge pull request #17 from PPPig0132/asus)
     }
 }
